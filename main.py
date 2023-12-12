b18734e933765a9261c25fee58a0bba2e5b3b9e0
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2023-12-12 12:33:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ class Circle():
         self.h = h
         self.w = w 
         self.x = x 
-        self.y = y 
+        self.y = y   
+        self.xspeed = random.randrange(-3, 3) 
+        self.yspeed = random.randrange(-3, 3)
     def drawCircle(self, screen):
         pygame.draw.ellipse(screen, GREEN, [self.x, self.y, self.w, self.h], 2)  
+    def moveCircle(self):
+        self.x += self.xspeed 
+        self.y += self.yspeed 
 
 # Rectangle 
 class Rect():
@@ -24,21 +29,25 @@ class Rect():
         self.y = y 
         self.w = w 
         self.h = h 
+        self.xspeed = random.randrange(-3, 3) 
+        self.yspeed = random.randrange(-3, 3)
     def drawRect(self, screen):
         pygame.draw.rect(screen, RED, [self.x, self.y, self.w, self.h], 2)
-
+    def moveRect(self):
+        self.x += self.xspeed 
+        self.y += self.yspeed
 
 # List of Circles 
 circles = [] 
 for i in range(10): 
     wHRan = random.randrange(30, 50)
-    circles.append(Circle(random.randrange(0, 800), random.randrange(0, 600), wHRan, wHRan))  
+    circles.append(Circle(random.randrange(0, 750), random.randrange(0, 550), wHRan, wHRan))  
 
 # List of Rectangles 
 rects = [] 
 for i in range(10):
     wHRan = random.randrange(30, 50)
-    rects.append(Rect(random.randrange(0, 800), random.randrange(0, 600), wHRan, wHRan))
+    rects.append(Rect(random.randrange(0, 750), random.randrange(0, 550), wHRan, wHRan))
 
 # Mouse Collision  
 def mouse_position():
@@ -54,7 +63,23 @@ def mouse_check(list):
     for i in range (len(list)):
         if mouseCollision(list[i]): 
             return i 
-    return -1 
+    return -1  
+
+# Wall Collision 
+def rectCollision(rect): 
+    if rect.x >= 800 - rect.w:
+        rect.xspeed = -rect.xspeed
+        rect.yspeed = -rect.yspeed  
+    elif rect.x <= 0:
+        rect.xspeed = -rect.xspeed
+        rect.yspeed = -rect.yspeed
+    elif rect.y >= 600 - rect.h:
+        rect.yspeed = -rect.yspeed
+        rect.xspeed = -rect.xspeed  
+    elif rect.y <= 0:
+        rect.yspeed = -rect.yspeed 
+        rect.xspeed = -rect.xspeed  
+   
 
 # Main function 
 def main():
@@ -67,7 +92,6 @@ def main():
  
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock() 
-    print(circles)
  
     # Main Program Loop 
     while not done:
@@ -78,15 +102,23 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 if mouse_check(circles) != -1:
                     circles.pop(mouse_check(circles))  
+                if circles == []: 
+                    done = True
+                    print("Game Over - You Win.")
                 elif mouse_check(rects) != -1: 
-                    pygame.quit() 
-                    print("Game Over - You Lose.")
-        
+                    done = True
+                    print("Game Over - You Lose.") 
+
+        # Draw and Move 
         screen.fill(BLACK)
         for i in range(len(circles)):
-            circles[i].drawCircle(screen)  
+            circles[i].drawCircle(screen) 
+            circles[i].moveCircle()  
+            rectCollision(circles[i])
         for i in range(len(rects)): 
-            rects[i].drawRect(screen)
+            rects[i].drawRect(screen) 
+            rects[i].moveRect() 
+            rectCollision(rects[i])
         pygame.display.flip() 
     
         # --- Limit frames
